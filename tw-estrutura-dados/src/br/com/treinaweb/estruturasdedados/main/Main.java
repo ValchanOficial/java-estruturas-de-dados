1cbcf2a9233c50254366bb3eb6aa2a0c3fb0ab6d
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-12-13 15:16:21 -0200

    Vetores pt 1

--- tw-estrutura-dados/src/br/com/treinaweb/estruturasdedados/main/Main.java
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturasdedados/main/Main.java
@@ -1,19 +1,23 @@
 package br.com.treinaweb.estruturasdedados.main;
 
 import java.util.Scanner;
-
 import br.com.treinaweb.estruturasdedados.modelos.Pessoa;
+import br.com.treinaweb.estruturasdedados.vetores.Vetor;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Digite a opção desejada: ");
 		System.out.println("1. Gerenciamento de memória");
+		System.out.println("2. Vetores");
 		Scanner scanner = new Scanner(System.in);
 		int opcao = scanner.nextInt();
 		switch(opcao) {
 		case 1:
 			fazerGerenciamentoMemoria();
+			break;
+		case 2:
+			fazerVetor();
 			break;
 		}
 		scanner.close();
@@ -42,4 +46,22 @@ public class Main {
 		System.out.println(p1==p2); //compara posição na memória
 		System.out.println(p1.equals(p2)); //compara valores
 	}
+	
+	private static void fazerVetor() {
+//		Vetor<Pessoa> vetorPessoas = new Vetor<Pessoa>(3);
+//		vetorPessoas.inserirEm(0, new Pessoa(1, "TreinaWeb"));
+//		System.out.println(vetorPessoas.recuperar(0).getNome());
+//		//------------
+//		Vetor<Integer> vetorInteiros = new Vetor<Integer>(1);
+//		vetorInteiros.inserirEm(0, 1);
+//		System.out.println(vetorInteiros.recuperar(0));
+		Vetor<Pessoa> vetorPessoas = new Vetor<Pessoa>();
+		vetorPessoas.inserir(new Pessoa(1, "TreinaWeb 1"));
+		vetorPessoas.inserir(new Pessoa(2, "TreinaWeb 2"));
+		vetorPessoas.inserir(new Pessoa(3, "TreinaWeb 3"));
+		vetorPessoas.inserir(new Pessoa(4, "TreinaWeb 4"));
+		vetorPessoas.inserirEm(1, new Pessoa(5, "TreinaWeb 5"));
+		System.out.println(vetorPessoas);
+	}
+	
 }
---
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturasdedados/vetores/Vetor.java
@@ -0,0 +1,55 @@
+package br.com.treinaweb.estruturasdedados.vetores;
+
+import java.util.Arrays;
+
+public class Vetor<T> {
+
+	private Object[] elementos;
+	private int posicao;
+	
+	public Vetor(int capacidade) {
+		this.elementos = new Object[capacidade];
+		this.posicao = 0;
+	}
+	
+	public Vetor() {
+		this.elementos = new Object[3];
+		this.posicao = 0;
+	}
+	
+	public void inserir(T elemento) {
+		if(this.posicao >= this.elementos.length) {
+			this.elementos = Arrays.copyOf(this.elementos, this.elementos.length+1);
+		}
+		this.elementos[this.posicao] = elemento;
+		this.posicao++;
+	}
+	
+	public void inserirEm(int posicao, T elemento) {
+		if (posicao > this.elementos.length) {
+			throw new IllegalArgumentException(String.format("A posição é inválida [%d]", posicao));
+		}
+		if (this.elementos[posicao] != null) {
+			Object[] arrayFinal = Arrays.copyOfRange(this.elementos, posicao, this.elementos.length);
+			Object[] arrayInicio = new Object[posicao+1];
+			System.arraycopy(this.elementos, 0, arrayInicio, 0, posicao);
+			arrayInicio[arrayInicio.length-1] = elemento;
+			int novoTamanho = arrayFinal.length + arrayInicio.length;
+			this.elementos = new Object[novoTamanho];
+			System.arraycopy(arrayInicio, 0, this.elementos, 0, arrayInicio.length);
+			System.arraycopy(arrayFinal, 0, this.elementos, arrayInicio.length, arrayFinal.length);
+		} else {
+			this.elementos[posicao] = elemento;	
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T recuperar(int posicao) {
+		return (T)this.elementos[posicao];
+	}
+
+	@Override
+	public String toString() {
+		return "Vetor [elementos=" + Arrays.toString(elementos) + "]";
+	}
+}
