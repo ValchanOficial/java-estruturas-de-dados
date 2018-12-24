51c14230fa319713d5138dcadabfa6da5ff9a3ea
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-12-24 17:04:16 -0200

    Iterando sob um vetor

--- tw-estrutura-dados/src/br/com/treinaweb/estruturasdedados/vetores/Vetor.java
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturasdedados/vetores/Vetor.java
@@ -45,7 +45,14 @@ public class Vetor<T> {
 	
 	@SuppressWarnings("unchecked")
 	public T recuperar(int posicao) {
+		if(posicao >= tamanho()) {
+			throw new IllegalArgumentException(String.format("Posição inválida! [%d]", posicao));
+		}
 		return (T)this.elementos[posicao];
+	}
+	
+	public int tamanho() {
+		return this.elementos.length;
 	}
 
 	@Override
